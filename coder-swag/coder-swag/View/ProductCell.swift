dbb869d8f475ec93f3a85ca05196b15dd20bf431
David Roff <David.Roff@asynchrony.com>
2018-01-30 14:09:18 -0600

    built the product view

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by david.roff on 1/30/18.
+//  Copyright © 2018 david.roff. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
